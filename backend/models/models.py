ec8c3af160fe5fe55fa2cbcc4cf09ed1a6b673b4
58divya <divyalamichhane85@gmail.com>
2025-06-30 01:27:16 +1200

    Added backend/models/ for more modularity

---
+++ backend/models/models.py
@@ -0,0 +1,27 @@
+from extensions import db
+from datetime import datetime, timezone
+
+class User(db.Model):
+    __tablename__ ='users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Contact(db.Model):
+    __tablename__ ='contact_records'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+class Booking(db.Model):
+    __tablename__ ='bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    advisor_id = db.Column(db.Integer, nullable=False)
+    advisor_name = db.Column(db.String(100), nullable=False)
+    user_name = db.Column(db.String(100), nullable=False)
+    user_email = db.Column(db.String(120), nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.String, nullable=False)  # 'YYYY-MM-DD'
+    time = db.Column(db.String, nullable=False)  # 'HH:MM'
