a117fbaa86a1c2859b04a72d61922870fdcc8bb6
58divya <divyalamichhane85@gmail.com>
2025-06-30 01:22:43 +1200

    Add backend/config.py

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # loads from .env in root folder
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///mydatabase.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret-jwt')
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = MAIL_USERNAME
