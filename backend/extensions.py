61e8fb838983c7576f4737f023c62baea16b30fd
58divya <divyalamichhane85@gmail.com>
2025-07-02 22:21:08 +1200

    Update: general modifications to app and config files

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_jwt_extended import JWTManager
+from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 mail = Mail()
